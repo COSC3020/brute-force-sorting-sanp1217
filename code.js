e08ffc11213fd839cb201ccb38e010ba8d68b9f9
pabs <pablosgrad12@gmail.com>
2023-09-25 21:27:16 -0600

    Not sure why the tests don't pass with [0, 0]

--- code.js
+++ code.js
@@ -1,3 +1,69 @@
 function permutationSort(a) {
-    return 0;
+    var count = 0;
+    var permutations = getPermutations(a);
+
+    console.log("orig input: " + a);
+
+    if (permutations.length <= 1) {
+        console.log("It took " + count + " permutations to find the sorted list");
+        return permutations;
+    }
+
+    //Checks each permutation to see if it's sorted.
+    for (var i = 0; i < permutations.length; i++) {
+        if (!isSorted(permutations[i])) {
+            count++;
+        } else {
+            console.log("It took " + count + " permutations tries to find the sorted list: " + permutations[i]);
+            return permutations[i];
+        }
+    }
+}
+
+function isSorted(a) {
+    if (a.length == 1) {
+        return true;
+    }
+
+    //Compares ith element to i - 1 element. It should be 
+    //smaller if it's sorted but if it's bigger, return false
+    //as one element is at least out of place.
+    for (var i = 1; i < a.length; i++) {
+        if (a[i - 1] > a[i]) {
+            return false
+        }
+    }
+
+    return true;
+}
+
+
+function getPermutations(a) {
+    var result = [];
+
+    //Base cases
+    if (a.length == 0) {
+        return [];
+    }
+    if (a.length == 1) {
+        return [a]
+    }
+
+    for (var i = 0; i < a.length; i++) {
+        //Take one num in the array
+        const currNum = a[i];
+
+        //Get all numbers except currNum
+        var remainingNums = a.slice(0, i).concat(a.slice(i + 1));
+
+        //Recursive call
+        var remainingNumsPermutated = getPermutations(remainingNums);
+
+        //Account for elements left
+        for (var j = 0; j < remainingNumsPermutated.length; j++) {
+            var permutedArray = [currNum].concat(remainingNumsPermutated[j]);
+            result.push(permutedArray);
+        }
+    }
+    return result;
 }
